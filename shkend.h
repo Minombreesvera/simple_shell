eb345eecc2bd8af7c428f1d7023f029b6a09704c
Minombreesvera <kehindebolaji25@gmail.com>
2023-11-07 20:44:54 -0800

    shell

--- shkend.h
+++ shkend.h
@@ -1,2 +1,11 @@
 #ifndef SHKEND_H
 #define SHKEND_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
